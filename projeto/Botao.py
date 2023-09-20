d38fe916ac2ce9f8a2c81635453edf3bd1d95e3e
Filipe Bezerra Muniz da Silva <fbms@WINDOWS.CIN.UFPE.BR>
2023-09-20 12:03:57 -0300

    coloquei os botoes e pausando o jogo, falta terminar menu

---
+++ projeto/Botao.py
@@ -0,0 +1,27 @@
+import pygame
+from settings import *
+
+class Button():
+    def __init__(self, x, y, image, scale) : #recebe como parametro as coordenadas x y, a imagem do botao e a escala da imagem
+        altura_botao = image.get_height()
+        largura_botao =  image.get_width()
+        self.image = pygame.transform.scale(image, (int(largura_botao *scale), int(altura_botao * scale)))
+        self.rect = self.image.get_rect() #retangulo da img
+        self.rect.topleft = (x,y)
+        self.clicado = False
+    
+    def draw(self, surface): #desenha na tela o botao e verifica se o mouse clicou nele a partir da posição e do click do mouse, retorna true para clicado
+        click = False
+        #desenhar botao na tela
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+        #posição do mouse
+        pos = pygame.mouse.get_pos()
+        
+        #chegando se mouse está sobre os botoes
+        if self.rect.collidepoint(pos):#collidepoint verifica se o 'rect' (retangulo da imagem do botao) colidiu com o ponto referente a posição do mouse
+          if pygame.mouse.get_pressed()[0] == 1 and self.clicado == False:#checando se enquanto estiver em cima da imagem do botao, o mouse foi pressioando bot esq
+            self.clicado = True
+            click = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicado = False
+        return click
--- projeto/main.py
+++ projeto/main.py
@@ -10,12 +10,22 @@ from debug import debug
 from collectibles import *
 from hud import HUD
 from tela_morte import Tela_morte
+from Botao import *
+
 # Inicializando o PyGame
 pygame.init()
 # Definindo o titulo e o icon para a janela onde o jogo será executado
 pygame.display.set_caption("Dungeon Sword")
 icone = pygame.image.load("projeto\sprites_folder\sprite_06.png")
 pygame.display.set_icon(icone)
+#carregando imagens dos botoes
+start_img = pygame.image.load('projeto/assets/start_btn.png').convert_alpha()
+exit_img = pygame.image.load('projeto/assets/exit_btn.png').convert_alpha()
+restart_img = pygame.image.load('projeto/assets/restart_btn.png').convert_alpha()
+#instanciando os botoes, atribuindo a eles a classe Button de Botao
+start_botao = Button((LARGURA_TELA/2 - start_img.get_width()) , 9* ALTURA_TELA /10 - (restart_img.get_height()/2), start_img, 0.75)#args: pos x, pos y , img, escala
+exit_botao = Button((LARGURA_TELA/2 - exit_img.get_width()) , 9* ALTURA_TELA /10 - (restart_img.get_height()/2), exit_img, 0.75) 
+restart_botao = Button(0,  9* ALTURA_TELA /10 - (restart_img.get_height()/2) ,restart_img, 0.3)
 
 
 class Game:
@@ -23,6 +33,7 @@ class Game:
         # Criando um objeto de relógio que ajudara a controlar o tempo percorrido no jogo
         self.relogio = pygame.time.Clock()
     def running(self):
+        jogo_pausado = True
         # Decidindo uma fonte para a UI, e criando a instancia de mapa, inimigo e jogador
         fonte = pygame.font.Font('projeto/assets/fonts\Pixeltype.ttf', 50)
         mapa = Mapa()
@@ -36,7 +47,52 @@ class Game:
                 # Cria o mapa do jogo
                 mapa.desenhar_mapa()
                 # Vai checar quais eventos estão ocorrendo por dentro
+                if jogo_pausado:
+                    #ir para menu
+                    if start_botao.draw(TELA):# se o botao foi pressionado executa ação
+                        jogo_pausado = False
+                        print('aaa')
+                    if exit_botao.draw(TELA):
+                        print('exit')
+                        break
+                else: 
+                     # Isso vai atualizar o jogador e o inimigo, vendo se o jogador fez algum input, se o jogador ou o inimigo sofreu dano, e movimentando ambos, e após isso tudo, coloca suas superficies na tela
+                    for inimigo in Enemy.lista_inimigos_presentes:
+                        inimigo.atualizar(jogador, mapa)
+                    if len(Enemy.lista_inimigos_presentes) == 0:
+                        TELA.blit(porta.imagem, porta.hitbox)
+                        if porta.hitbox.colliderect(jogador.hitbox):
+                            mapa.proxima_fase()
+                    jogador.atualizar(mapa)
+                    #A hud no momento esta só como uma imagem para conter o contador de vida e de quantos abates foram feitos"
+                    sup_vida= fonte_hud.render(f'{jogador.vida}', False, "Yellow")
+                    sup_pontuacao = fonte.render(f'{jogador.pontuacao}', False, 'White')
+                    sup_nivel = fonte.render(f'Nivel: {Enemy.onda}', False, 'White')
+                    sup_espada = fonte.render(f'Espada: {jogador.nivel_espada}', False, 'White')
+                    TELA.blit(sup_pontuacao, (205,100))
+                    TELA.blit(sup_nivel, (80,150))
+                    TELA.blit(sup_espada, (50,200))
+                    TELA.blit(HUD.hud, (2,2))
+                    TELA.blit(sup_vida, (225,43))
+                    #Se o jogador morrer ele vai mostrar a tela de morte
+                    if jogador.vida == 0:
+                        TELA.blit(Tela_morte.tela_morte, (1,1))
+                        TELA.blit(sup_pontuacao, (600,400))
+                        TELA.blit(sup_nivel, ( 550,450))
+                        if restart_botao.draw(TELA):
+                            print('abacaxi')
+                        if exit_botao.draw(TELA):
+                            print('exit')
+                            break 
+                    # Debug
+                    # Atualizando o que aparece na tela a cada "Tick" (Tick é uma única atualização que ocorre na simulação do jogo)
+                    # Limitando o número máximo de 'ticks'/'frames' por segundo a 60 para evitar que ocorra atualizações excessivas
+                    self.relogio.tick(FPS)
                 for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_m:
+                            jogo_pausado = True
+                            print('asdasdas')
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         exit()
@@ -52,34 +108,8 @@ class Game:
                     TELA.blit(coletavel.imagem, coletavel.hitbox) #insere coletáveis
                     coletavel.coletar(jogador)
 
-                # Isso vai atualizar o jogador e o inimigo, vendo se o jogador fez algum input, se o jogador ou o inimigo sofreu dano, e movimentando ambos, e após isso tudo, coloca suas superficies na tela
-                for inimigo in Enemy.lista_inimigos_presentes:
-                    inimigo.atualizar(jogador, mapa)
-                if len(Enemy.lista_inimigos_presentes) == 0:
-                    TELA.blit(porta.imagem, porta.hitbox)
-                    if porta.hitbox.colliderect(jogador.hitbox):
-                        mapa.proxima_fase()
-                jogador.atualizar(mapa)
-                #A hud no momento esta só como uma imagem para conter o contador de vida e de quantos abates foram feitos"
-                sup_vida= fonte_hud.render(f'{jogador.vida}', False, "Yellow")
-                sup_pontuacao = fonte.render(f'{jogador.pontuacao}', False, 'White')
-                sup_nivel = fonte.render(f'Nivel: {Enemy.onda}', False, 'White')
-                sup_espada = fonte.render(f'Espada: {jogador.nivel_espada}', False, 'White')
-                TELA.blit(sup_pontuacao, (205,100))
-                TELA.blit(sup_nivel, (80,150))
-                TELA.blit(sup_espada, (50,200))
-                TELA.blit(HUD.hud, (2,2))
-                TELA.blit(sup_vida, (225,43))
-                #Se o jogador morrer ele vai mostrar a tela de morte
-                if jogador.vida == 0:
-                    TELA.blit(Tela_morte.tela_morte, (1,1))
-                    TELA.blit(sup_pontuacao, (600,400))
-                    TELA.blit(sup_nivel, ( 550,450))
-                # Debug
-                # Atualizando o que aparece na tela a cada "Tick" (Tick é uma única atualização que ocorre na simulação do jogo)
+               
                 pygame.display.update()
-                # Limitando o número máximo de 'ticks'/'frames' por segundo a 60 para evitar que ocorra atualizações excessivas
-                self.relogio.tick(FPS)
 # Certificando que o jogo só será rodado nesse arquivo, e não caso ele seja importado ou algo do tipo
 if __name__ == '__main__':    
     Game().running()
